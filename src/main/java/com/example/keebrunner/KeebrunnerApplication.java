d8062a436fdfd6cdbdb3c68d12151eabb3e39000
keebrunner <keebrunner@gmail.com>
2024-07-14 15:18:44 +0400

    commit

---
+++ src/main/java/com/example/keebrunner/KeebrunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.keebrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeebrunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeebrunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keebrunner/KeebrunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keebrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeebrunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
